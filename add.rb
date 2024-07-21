a08d4550a3e3d97cd84ee514d7700af4295b6068
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:46:36 +0900

    Update add.rb

--- add.rb
+++ add.rb
@@ -1,1 +1,22 @@
+require_relative 'analyzer'
+require_relative 'categories'
+require_relative 'category'
+require_relative 'doc'
+
+class App
+    def initialize
+        @analyzer = Analyzer.new
+        @grabber = Grabber.new
+    end
+    attr_reader :analyzer
+
+    def train(text, category)
+        analyzer.train(text, category)
+    end
+    
+    def classify(url_or_file)
+        text = grabber.grab(url_or_file)
+        analyzer.classify(text)
+    end
+end
 
