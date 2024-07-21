79212947e2538ffd9ab623b1590b1aa1fd543a68
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:46:59 +0900

    Update analyzer.rb

--- analyzer.rb
+++ analyzer.rb
@@ -1,1 +1,16 @@
+require_relative 'categories'
+require_relative 'category'
+
+class Analyzer
+    def train(text, category_name)
+        categories = Categories.new
+        category = categories.search(category_name)
+        category.train(text)
+    end
+
+    def classify(text)
+        categories = Categories.new
+        categories.classify(text)
+    end
+end
 
