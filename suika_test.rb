ee617058694637ddd12c3c5339b548665474da23
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:52:50 +0900

    Update suika_test.rb

--- suika_test.rb
+++ suika_test.rb
@@ -1,1 +1,22 @@
+require 'suika'
 
+tagger = Suika ::Tagger.new
+
+text = "すもももももももものうち"
+
+resalt = tagger.parse(text)
+resalt.each do |token|
+    puts token
+end
+
+
+require 'suika'
+
+tagger = Suika ::Tagger.new
+
+text = "This is simple program."
+
+resalt = tagger.parse(text)
+resalt.each do |token|
+    puts token
+end
