ade44f43fac217d83113610a489210b25e862cdb
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:47:26 +0900

    Update categories_test.rb

--- categories_test.rb
+++ categories_test.rb
@@ -1,1 +1,12 @@
 
+require_relative 'categories'
+
+categories = Categories.new
+puts categories.category_list.inspect
+
+categories.search(:spam)
+puts categories.category_list.inspect
+
+categories.search(:normal)
+puts categories.category_list.inspect
+
