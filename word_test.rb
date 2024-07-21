c40db23c0580028c018c8bf9008807fce95e838a
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:53:47 +0900

    Update word_test.rb

--- word_test.rb
+++ word_test.rb
@@ -1,1 +1,12 @@
+require_relative 'word'
+require 'suika'
+
+tagger =  Suika::Tagger.new
+lines = tagger.parse('わたしは京都にいます。')
+
+word = Word.new(lines[0])
+puts word.is_noun? || word.is_verb?
+puts word.word.inspect
+puts word.attributes.inspect
+puts word.attribute_list.inspect
 
