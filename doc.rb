b57e5ad5c62e709f0670caee8f7febbd0676cab2
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:49:49 +0900

    Update doc.rb

--- doc.rb
+++ doc.rb
@@ -1,1 +1,30 @@
+class Document
+    def initialize(text)
+    @text = text
+    end
+
+    attr_reader :text
+
+    def words
+        words.strip.split(/\s+/)
+    end
+end
+
+
+class Doc
+    def initialize(text)
+    @text = text
+    end
+
+    attr_reader :text
+
+    def words
+        words_selector = WordsSelector.new
+    text = "私は京都で働いたことがあります。"
+    result = words_selector.parse(text)
+
+        text.strip.split(/\s+/)
+    end
+end
+
 
