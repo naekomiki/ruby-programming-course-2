fceaf1bf8e37bd07c2434e6723d297eae2cc3fe3
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:50:37 +0900

    Update grabber.rb

--- grabber.rb
+++ grabber.rb
@@ -1,1 +1,22 @@
+require 'open-uri'
+require 'nokogiri'
 
+class Grabber
+    def grab_from_uri(uri)
+        html = URI.open(uri)
+        remove_tags(html)
+    end
+
+    def grab_from_file(file)
+      File.open(file).read
+    end
+
+    def grab_from_htmlfile(html_file)
+        html = File.open(html_file).read
+        remove_tags(html)
+    end
+
+    private
+    def remove_tags(html)
+        doc = Nokogiri::HTML.parse(html)
+    end
