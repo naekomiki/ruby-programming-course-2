d098de2cff6710c0abe1e3d145c08be7444d1bb5
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:54:21 +0900

    Update words_selector_test.rb

--- words_selector_test.rb
+++ words_selector_test.rb
@@ -1,1 +1,22 @@
+require_relative 'words_selector'
+
+words_selector =WordsSelector.new
+resalt = words_selector.parse('This is simple program.')
+puts result.inspect
+
+
+require_relative 'words_selector'
+
+words_selector =WordsSelector.new
+text = "これは日本語の文章です。"
+resalt = words_selector.parse(text)
+puts result.inspect
+
+
+require_relative 'words_selector'
+
+words_selector =WordsSelector.new
+text = "私は京都で働いたことがあります。"
+resalt = words_selector.parse(text)
+puts result.inspect
 
