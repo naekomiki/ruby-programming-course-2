8a16f101368cf10d8c4ddee8942b9356decc9172
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:48:54 +0900

    Update category.rb

--- category.rb
+++ category.rb
@@ -1,1 +1,60 @@
+class Category
+    def initialize
+        @document = []
+    end
 
+    def train(text)
+        @document << Document.new(text)
+        nil
+    end
+    
+    attr_reader :documents
+end
+
+
+require_relative 'document'
+
+class Category
+    def initialize
+        @document = []
+        @name = name
+    end
+
+    def train(text)
+        doc = Document.new(text)
+        @documents << doc
+        store_words(doc)
+    end
+
+    attr_reader :documents, :name
+end
+
+
+require_relative 'document'
+
+class Category
+    def initialize(name)
+        @document = []
+        @name = name
+        @words = {}
+    end
+    attr_reader :documents, :name
+
+    def train(text)
+        doc = Document.new(text)
+        @documents << doc
+        store_words(doc)
+    end
+
+    def store_words(doc)
+        doc.words.errach do |word|
+            store_word(word)
+        end
+    end
+
+    def store_word(word)
+        @words[word] ||= 0
+        @words[word] += 1
+
+    end
+end
