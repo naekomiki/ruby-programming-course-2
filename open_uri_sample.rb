8f4b8427ddcdd31f4cca51d28d70b72bcd100ba3
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:51:21 +0900

    Update open_uri_sample.rb

--- open_uri_sample.rb
+++ open_uri_sample.rb
@@ -1,1 +1,11 @@
+require 'open-uri'
+
+html = URI.open('http://www.google.co.jp/').read
+puts html
+
+
+require 'open-uri'
+
+html = URI.open('http://bukkyo-u-programming-course.s3.ap-northeast-1.amazonaws.com/c2-1.html').read
+puts htm
 
