399e262963de809b03dc0ca67905feb415bc06fb
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:53:30 +0900

    Update word.rb

--- word.rb
+++ word.rb
@@ -1,1 +1,39 @@
+class Word
+    def initialize(line)
+        @line = line
+    end
+    attr_reader :line
+
+    def is_noun?
+        attribute_list[0] == '名詞'
+    end
+
+    def is_verb?
+        attribute_list[0] == '動詞'
+    end
+
+    def word
+        line.split(/\t/)[0]
+    end
+
+    def attributes
+        line.split(/\t/)[1]
+    end
+  
+    def original
+        attribute_list[6]
+    end
+
+    def word
+        line.split(/\t/)[0]
+    end
+
+    def attributes
+        line.split(/\t/)[1]
+    end
+
+    def attribute_list
+        attributes.split(',')
+    end
+end
 
