e3712eaefd3a25f716a81a6afa04363d559f9999
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-21 10:55:27 +0900

    Update words_splitter.rb

--- words_splitter.rb
+++ words_splitter.rb
@@ -1,1 +1,24 @@
+class WordsSelector
+    def initialize
+        @@tagger ||= Suika
+    end
 
+    def self.split(line)
+        array = tagger.parse(text)
+        words = array.map do |line|
+    end
+end
+
+
+class WordsSelector
+    def initialize
+        @@tagger ||= Suika::Tagger.new
+    end
+
+    def self.split(line)
+        array = @@tagger.parse(text)
+        words = array.map do |line|
+            Word.new(line)
+        end
+    end
+end
