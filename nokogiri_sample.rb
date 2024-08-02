5c2a2a87d7e8ffdc38730112c9a7f78ecfcf1a4a
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-08-02 09:22:41 +0900

    Update nokogiri_sample.rb

--- nokogiri_sample.rb
+++ nokogiri_sample.rb
@@ -1,4 +1,8 @@
+require 'open-uri'
 require 'nokogiri'
+
+html = URI.open('https://bukkyo-u-programming-course.s3.ap-northeast-1.amazonaws.com/c2-1.html').read
+puts html
 
 doc = Nokogiri::HTML.parse(html)
 puts doc.text
